e507fa6cada8b90208cea186bda59c3e70177814
Mengqi Zhu <treeforever7@gmail.com>
2015-09-26 10:52:30 -0400

    highlight selected ingredient and creat ingredientCounterMessage
    change background-color when ingredient is selected
    
    send ingredientCounterMessage

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
   $('img').click(function(){
-    $("img").hide()
-  })
+    $("img").hide();
+  });
+});
+
+$(document).ready(function() {
+  $('ul#ingredients-list li').click(function() {
+    var $this = $(this);
+    var numCheckedCheckbox = $this.find('input[type=checkbox]:checkbox:checked').length;
+    var checked = (numCheckedCheckbox === 1);
+    if(checked) {
+      $this.css('background-color', 'yellow');
+    }
+    else {
+      $this.css('background-color', '');
+    }
+  });
+
+  $('ul#ingredients-list li').click(function() {
+    var numChecked = $('ul#ingredients-list').find('input[type=checkbox]:checkbox:checked').length;
+    var ingredientCounterMessage;
+    if(numChecked > 0) {
+      ingredientCounterMessage = numChecked + ' ingredients selected.';
+    }
+    else {
+      ingredientCounterMessage = 'No ingreident selected.';
+    }
+    $('#ingredient-counter').text(ingredientCounterMessage);
+  });
 });
